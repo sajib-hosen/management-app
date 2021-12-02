0e8ce9ecf0c173944f37709d82bdc6f98f50052c
Sajib Hosen <sajib.201h@gmail.com>
2021-12-02 19:06:46 +0600

    updated

--- src/components/Pages/SetEmployees/SetEmployees.js
+++ src/components/Pages/SetEmployees/SetEmployees.js
@@ -24,7 +24,7 @@ const SetEmployees = () => {
             <div>
                 <Routes>
                     <Route path="/" element={ <p className='border-2'>welcome</p>} />
-                    <Route path="add-employee" element={ <AddEmployee/>} />
+                    <Route path="add-employee" element={<AddEmpRoute> <AddEmployee/> </AddEmpRoute> } />
                     <Route path="list-of-employess" element={<p className='border-2'>This is list of employees</p>} />
                     <Route path="manage-employee" element={<p className='border-2'>Manage employess</p>} />
                     <Route path="proprietor" element={<DefaultAdminRoute> <AddProprietor /> </DefaultAdminRoute>} />
